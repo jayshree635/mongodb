3551c6d9f617bf2036f0909b44fed1227ae69754
jayshree <jayshree.webappdev@gmail.com>
2023-07-04 16:17:54 +0530

    change

--- controller/user/user.controller.js
+++ controller/user/user.controller.js
@@ -13,7 +13,8 @@ const UserRegistration = async (req, res) => {
         address: 'required|string'
     })
     
-    if (validation.fails()) {
+    if (validation.fails()) { 
+        
         firstMessage = Object.keys(validation.errors.all())[0];
         return RESPONSE.error(res, validation.errors.first(firstMessage))
     }
